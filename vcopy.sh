e38c0c018a866b9fbc6bbbbbca2f24a010f59690
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2022-06-23 13:36:30 -0500

    chown ${ISC_PACKAGE_MGRUSER}:${ISC_PACKAGE_IRISGROUP} /usr/local/etc/irissys/iris.reg

--- vcopy.sh
+++ vcopy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# [INFO] Starting InterSystems IRIS instance IRIS...
+# [INFO] Invalid registry ownership
+# [ERROR] Command "iris start IRIS quietly" exited with status 256
+##RUN chown ${ISC_PACKAGE_MGRUSER}:${ISC_PACKAGE_IRISGROUP} /usr/local/etc/irissys/iris.reg
+if [ -f "/usr/local/etc/irissys/iris.reg" ]; then
+    chown ${ISC_PACKAGE_MGRUSER}:${ISC_PACKAGE_IRISGROUP} /usr/local/etc/irissys/iris.reg
+fi
+
 if [ ! -d "/voldata/irisdb/irisapp" ]; then
     mkdir -p "/voldata/irisdb/irisapp"
     if [ -d "/ghostdb" ]; then
