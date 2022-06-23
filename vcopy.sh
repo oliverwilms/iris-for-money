8bbd45f075bfcea2fce61b3fb79a7ccb4080a9bb
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2022-06-22 21:39:43 -0500

    Update vcopy.sh

--- vcopy.sh
+++ vcopy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cp -Rpfv /ghostdb/* /voldata/
+if [ ! -d "/voldata/irisdb/irisapp" ]; then
+    mkdir -p "/voldata/irisdb/irisapp"
+    if [ -d "/ghostdb" ]; then
+        cp -Rpfv /ghostdb/* /voldata/
+    fi
+fi
 exit $?
